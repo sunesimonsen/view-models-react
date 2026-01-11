afa7291fb2e7bc0a46e0d2ae0ebb0cc765131f64
Sune Simonsen <sune@we-knowhow.dk>
2026-01-11 15:06:54 +0100

    Replace view model dependency with interface

--- src/useModelState.ts
+++ src/useModelState.ts
@@ -1,5 +1,41 @@
 import { useSyncExternalStore } from "react";
-import type { ViewModel, State } from "@view-models/core";
+
+/**
+ * Function that gets called when the state changes.
+ *
+ * @template T - The state type
+ * @param state - The new state
+ */
+export type ViewModelListener = () => void;
+
+export interface ViewModel<T> {
+  /**
+   * Subscribe to state changes.
+   *
+   * The listener will be called immediately after any state update.
+   *
+   * @param listener - Function to call when state changes
+   * @returns Function to unsubscribe the listener
+   *
+   * @example
+   * ```typescript
+   * const unsubscribe = viewModel.subscribe((state) => {
+   *   console.log('State changed:', state);
+   * });
+   *
+   * // Later, when you want to stop listening:
+   * unsubscribe();
+   * ```
+   */
+  subscribe(listener: ViewModelListener): () => void;
+
+  /**
+   * Get the current state.
+   *
+   * @returns The current state
+   */
+  get state(): T;
+}
 
 /**
  * A React hook that subscribes a component to a ViewModel's state updates.
@@ -30,6 +66,6 @@ import type { ViewModel, State } from "@view-models/core";
  * }
  * ```
  */
-export function useModelState<T extends State>(model: ViewModel<T>): T {
+export function useModelState<T>(model: ViewModel<T>): T {
   return useSyncExternalStore(model.subscribe.bind(model), () => model.state);
 }
